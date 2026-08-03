1ec9249f142de6c384f003469d3718dc3812f34a
Adnan Pal <paladnan70930@gmail.com>
2026-08-03 16:38:15 +0530

    Time: 1 ms (24.35%), Space: 43.6 MB (44.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int n = nums.length;
+        for(int i = 0;i<n;i++){
+            int minIndex = i;
+
+            for(int j = i+1;j<n;j++){
+                if(nums[j]<nums[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        
+    }
+}
