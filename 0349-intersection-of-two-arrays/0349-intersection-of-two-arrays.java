a90ca8c6dfd5aab0c85dd320b429d9b01190550b
Adnan Pal <paladnan70930@gmail.com>
2026-09-05 02:39:45 +0530

    Time: 9 ms (7.04%), Space: 44.4 MB (97.66%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        sort(nums1);
+        sort(nums2);
+
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int count = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            int target = nums1[i];
+
+            if (binarySearch(nums2, target)) {
+
+                // avoid duplicate elements
+                if (count == 0 || temp[count - 1] != target) {
+                    temp[count] = target;
+                    count++;
+                }
+            }
+        }
+
+        int[] result = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            result[i] = temp[i];
+        }
+
+        return result;
+    }
+
+    public static boolean binarySearch(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void sort(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+
+                if (nums[j] > nums[j + 1]) {
+
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
